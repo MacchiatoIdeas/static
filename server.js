baacf50e51da639af397c89dda74d7973cca5b6f
Hernán Herreros Niño <hernan.herreros.13@sansano.usm.cl>
2017-10-01 00:11:34 -0300

    Small changes

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const static = require('node-static');
 
 const file = new static.Server({
-	rootPath: './public',
+	rootPath:'./public',
 	headers: {
-	    'Access-Control-Allow-Origin': '*',
-	    'Access-Control-Allow-Methods': 'GET',
-	    'Access-Control-Allow-Headers': 'Content-Type'
-  		}
-	});
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Methods': 'GET',
+		'Access-Control-Allow-Headers': 'Content-Type'
+	}
+});
 
 require('http').createServer(function (request, response) {
   request.addListener('end', function () {
