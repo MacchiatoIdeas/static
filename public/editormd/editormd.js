7f24f1465b48f2eba4530eee8ba5468c277d7b2d
Hernán Herreros Niño <hernan.herreros.13@sansano.usm.cl>
2017-10-29 20:45:26 -0300

    Change editormd

--- public/editormd/editormd.js
+++ public/editormd/editormd.js
@@ -3600,11 +3600,11 @@
         markedRenderer.paragraph = function(text) {
             var isTeXInline     = /\$\$(.*)\$\$/g.test(text);
             var isTeXLine       = /^\$\$(.*)\$\$$/.test(text);
-            var isTeXAddClass   = "";
+            var isTeXAddClass   = (isTeXLine)     ? " class=\"" + editormd.classNames.tex + "\"" : "";
             var isToC           = (settings.tocm) ? /^(\[TOC\]|\[TOCM\])$/.test(text) : /^\[TOC\]$/.test(text);
             var isToCMenu       = /^\[TOCM\]$/.test(text);
             
-            /*if (!isTeXLine && isTeXInline) 
+            if (!isTeXLine && isTeXInline) 
             {
                 text = text.replace(/(\$\$([^\$]*)\$\$)+/g, function($1, $2) {
                     return "<span class=\"" + editormd.classNames.tex + "\">" + $2.replace(/\$/g, "") + "</span>";
@@ -3613,7 +3613,7 @@
             else 
             {
                 text = (isTeXLine) ? text.replace(/\$/g, "") : text;
-            }*/
+            }
             
             var tocHTML = "<div class=\"markdown-toc editormd-markdown-toc\">" + text + "</div>";
             
